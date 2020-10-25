edc1e127978ca71fe1cd94f7e23170c16631a5b2
MartoETuk <martin_shishkovski@abv.bg>
2020-10-25 17:03:01 +0000

    zavurshen kod

--- src/exercise4/ArrayOutput.java
+++ src/exercise4/ArrayOutput.java
@@ -3,7 +3,7 @@ package exercise4;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class ArreyOutput {
+public class ArrayOutput {
 
   public static void main(String[] args){
     Scanner input = new Scanner(System.in);
